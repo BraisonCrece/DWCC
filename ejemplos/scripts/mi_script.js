c962c46af11998c451efac420deec850ebd148be
A21BraisFD <A21BraisFD@iessanclemente.net>
2022-09-14 20:08:19 +0200

    "dia 1"

---
+++ ejemplos/scripts/mi_script.js
@@ -0,0 +1,10 @@
+let nombre = "Manolo"
+console.log(`Hola ${nombre}`)
+
+let codigo = `<ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>`
+
